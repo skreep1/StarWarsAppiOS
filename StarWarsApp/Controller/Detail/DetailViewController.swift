a2a5cce56d6c0259cd1946f1a11a4dc06860e752
Maksim Skrypka <skrypka.m.l@gmail.com>
2022-07-20 23:22:41 +0300

    new design for detail

--- StarWarsApp/Controller/Detail/DetailViewController.swift
+++ StarWarsApp/Controller/Detail/DetailViewController.swift
@@ -8,17 +8,13 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var heightLabel: UILabel!
-    @IBOutlet weak var weightLabel: UILabel!
-    @IBOutlet weak var genderLabel: UILabel!
-    @IBOutlet weak var birthdayLabel: UILabel!
-    @IBOutlet weak var hairLabel: UILabel!
-    @IBOutlet weak var skinLabel: UILabel!
-    @IBOutlet weak var eyeLabel: UILabel!
+    @IBOutlet weak var hair: UILabel!
+    @IBOutlet weak var birth: UILabel!
     
-    
+    @IBOutlet weak var skin: UILabel!
+    @IBOutlet weak var eye: UILabel!
     var people: People?
     
     override func viewDidLoad() {
@@ -28,15 +24,14 @@ class DetailViewController: UIViewController {
     }
     
     private func setupText() {
-        self.heightLabel.text = people?.height
+        //self.heightLabel.text = people?.height
         self.nameLabel.text = people?.name
-        self.weightLabel.text = people?.weight
-        self.genderLabel.text = people?.gender
-        self.birthdayLabel.text = people?.birth
-        self.hairLabel.text = people?.hair
-        self.skinLabel.text = people?.skin
-        self.eyeLabel.text = people?.eye
+//        self.weightLabel.text = people?.weight
+//        self.genderLabel.text = people?.gender
+        self.birth.text = people?.birth
+        self.hair.text = people?.hair
+        self.skin.text = people?.skin
+        self.eye.text = people?.eye
     }
-    
 
 }
